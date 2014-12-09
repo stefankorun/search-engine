f03f425990594a6e888def542094730a64eeb8a3
Stefan Korunovski <korun_stefan@hotmail.com>
2014-12-09 16:13:07 +0000

    dodaena kajgana arhiva

--- app/http-scrap.js
+++ app/http-scrap.js
@@ -4,25 +4,28 @@ var fs = require('fs');
 var cheerio = require('cheerio')
 
 var config = {
-    baseUrl: 'http://kurir.mk/makedonija/vesti/',
-    baseName: 'kurir',
-    reqStart: 140000,
-    reqEnd: 181524 // number of requests
+    baseUrl: 'http://kajgana.com/node/',
+    baseName: 'kajgana',
+    reqStart: 90000,
+    reqEnd: 100000 // number of requests
 };
 for (var i = config.reqStart; i < config.reqEnd; i++) {
     sendRequest(config.baseUrl, i);
+    
 }
 function sendRequest(baseUrl, i) {
     request.get(baseUrl + i + '/', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var html = cheerio.load(body);
-            var text = html('.article p').text();
+            var text = html('.field-items p').text();
             if (text) {
                 fs.writeFile("db/html/" + config.baseName + i, text, function (err) {
                     if (err) console.log(err);
                     else console.log(i, "The file was saved!");
                 });
             }
+        } else {
+            console.log(error);
         }
     });
 }
