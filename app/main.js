1a1e220bd50182eeec045ac1b82fe6bbe81afe05
Stefan <korun_stefan@hotmail.com>
2014-12-20 02:24:46 +0100

    rekurzija nekoja

--- app/main.js
+++ app/main.js
@@ -1,4 +1,9 @@
+var fs = require('fs');
+var _ = require('lodash');
+var request = require("request");
+var cheerio = require('cheerio');
+
 (function () {
   var ws = require('./web-scrape/web-scrape');
-  ws.testPublicFunction();
+  ws.startWebScrape('http://www.kajgana.com/', 2);
 })();
--- app/web-scrape/web-scrape.js
+++ app/web-scrape/web-scrape.js
@@ -1,29 +1,46 @@
 (function () {
-  var request = require("request");
+  // requires
   var fs = require('fs');
+  var _ = require('lodash');
+  var request = require("request");
   var cheerio = require('cheerio');
 
   var webScrape = {};
+  var urlRegex = /(https?:\/\/)(www\.)?(\w+\.?){1,2}(\.\w{2,5}){1,2}/g;
 
-  webScrape.testPublicFunction = function () {
-    var urlRegex = /(https?:\/\/)?(www\.)?(\w+\.?){1,2}(\.\w{2,5}){1,2}/g;
-    request.get('http://www.forum.kajgana.com/', function (error, response, body) {
+  var globalLinks = [];
+
+  webScrape.startWebScrape = function (startDomain, levelLimit) {
+    console.log('Starting web scraping on:', startDomain);
+
+    request.get(startDomain, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var html = cheerio.load(body);
         var aTags = html('a');
         var links = [];
+
         for (var i = 0; i < aTags.length; ++i) {
           var link = aTags.eq(i).attr('href') || '';
           var linkMatch = link.match(urlRegex);
-          if (linkMatch) console.log(linkMatch);
+          if (linkMatch) links.push(linkMatch[0]);
         }
+        links = _.uniq(links);
+        globalLinks.push(links);
 
-        console.log(links);
+        if (levelLimit) {
+          links.forEach(function (link) {
+            webScrape.startWebScrape(link, levelLimit - 1);
+          });
+        }
       } else {
         console.log(error);
       }
     });
   };
+
+  setTimeout(function () {
+    console.log(_.union.apply(this, globalLinks));
+  }, 5000);
 
   module.exports = webScrape;
 })();
