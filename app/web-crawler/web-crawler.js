8053b8bb6b4600896cd5206ade9d289da620c1b4
Stefan K <korun_stefan@hotmail.com>
2015-01-22 14:28:45 +0100

    podelba na internal i external links

--- app/web-crawler/web-crawler.js
+++ app/web-crawler/web-crawler.js
@@ -6,45 +6,61 @@
   var request = require("request");
   var cheerio = require('cheerio');
 
-  var webScrape = {};
 
-  webScrape.startWebCrawler = function (startDomains, levelLimit) {
-    console.log('Starting web scraping on:', startDomains);
-    if (!_.isArray(startDomains)) startDomains = [startDomains];
-    _.each(startDomains, function (domain) {
-      crawlPage(domain, levelLimit, true)
-    });
-  };
+  // Public
+  var webScrape = {};
+  module.exports = webScrape;
   webScrape.getAllLinks = function () {
     var links = fs.readFileSync('web-crawler/links.txt', {encoding: 'utf8'});
     return links.split(';');
   };
+  webScrape.startWebCrawler = function (startDomains, levelLimit) {
+    console.log('Starting web scraping on:', startDomains);
+    if (!_.isArray(startDomains)) startDomains = [startDomains];
+    _.each(startDomains, function (domain) {
+      startCrawling(domain, levelLimit, true);
+    });
+  };
 
+// Private
+  var externalUrlRegex = /^(https?:\/\/)(www\.)?(\w+\.?){1,2}(\.\w{2,5}){1,2}/g;
+  var internalUrlRegex = /^(\/[^?#]+)(\?|#.*)?/g;
 
-  var urlRegex = /(https?:\/\/)(www\.)?(\w+\.?){1,2}(\.\w{2,5}){1,2}/g;
   var globalLinks = [];
   var beenThereDoneThat = [];
 
-  function crawlPage(pageLink, level) {
+  function startCrawling(pageLink, level) {
     if (_.contains(beenThereDoneThat, pageLink)) return;
     beenThereDoneThat.push(pageLink); // TODO ova da se optimizirat so globalLinks
 
+    console.log(pageLink);
     request.get(pageLink, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var aTags = cheerio.load(body)('a');
-        var links = [];
+        var links = {
+          external: [],
+          internal: []
+        };
 
         for (var i = 0; i < aTags.length; ++i) {
           var link = (aTags.eq(i).attr('href') || '').toLowerCase();
-          var linkMatch = link.match(urlRegex);
-          if (linkMatch) links.push(linkMatch[0]);
+
+          var internalLink = link.match(internalUrlRegex);
+          if (internalLink) {
+            links.internal.push(internalLink[0]);
+          } else {
+            var externalLink = link.match(externalUrlRegex);
+            if (externalLink) links.external.push(externalLink[0]);
+          }
         }
-        links = _.uniq(links);
-        globalLinks.push(links);
+        links.external = _.uniq(links.external);
+        links.internal = _.uniq(links.internal);
+        globalLinks.push(links.external);
 
         if (level > 0) {
-          links.forEach(function (link) {
-            crawlPage(link, level - 1);
+          links.internal.forEach(function (link) {
+            link = 'http://' + response.request.host + link;
+            startCrawling(link, level - 1);
           });
         } else {
           fs.writeFileSync('web-crawler/links.txt', _.union.apply(this, globalLinks).join(';'));
@@ -55,7 +71,25 @@
     });
   }
 
-  module.exports = webScrape;
+  function urlSamples(pageLink) {
+    request.get(pageLink, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var aTags = cheerio.load(body)('a');
+        var links = [];
+
+        for (var i = 0; i < aTags.length; ++i) {
+          var link = (aTags.eq(i).attr('href') || '').toLowerCase();
+          links.push(link);
+        }
+        console.log(_.uniq(links).join(';\n'));
+      } else {
+        console.log('network ERR: ', pageLink, error);
+      }
+    });
+  }
+  //urlSamples('http://off.net.mk/vesti/mislenja/ne-ljubovta-uveruvanjata-se-slepi');
+
+
 })();
 
 
